e9c66ba0538cb5845527d12da26725ff5ca4662d
dragonmaster784 <dev@maholla.com>
2025-10-27 00:00:39 +0200

    🌩️ Update cloudflare adapter for static serving.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thecodex.page",
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap(), tailwind()],
 });
